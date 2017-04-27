6a750125454a4c1c8c16058a1a69bf8cad3b5be5
ryankeeney <rwk005@gmail.com>
2017-04-27 12:27:34 -0600

    Comments revision.

--- Lesson3/Exercise3/R/LeapYearCalculator.R
+++ Lesson3/Exercise3/R/LeapYearCalculator.R
@@ -2,18 +2,20 @@
 #4/24/2017
 #Leap Year Calculator
 
-
+# Function to determine whether a year is a leap year or not.
 LeapYearCalculator <- function(x) {
-  # Function to determine whether a year is a leap year or not.
-  # x (numeric) is the year you were born
+    # Make sure X is numeric. 
   if (!is.numeric(x)) {
     stop("Use number class.")
-  } else { #x is numeric
+  } else { 
+      # See if X is divisible by 4. If not, is is a common year.
       if (x %%4 != 0) {
       print("This is a common year.")
+      # Then see if X is divisible by 100. If not, is is a leap year.
     } else if (x %% 100 != 0) {
       print("This is a leap year.")
-    } else if (x %% 400) {
+      # Then see if x is divisible by 400. If not is a common year.
+    } else if (x %% 400 != 0) {
       print("This is a common year.")
     } else
       print("This is a leap year")
