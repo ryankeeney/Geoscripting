a494eb390ac19ae01d78904647d157c7bc58754c
ryankeeney <rwk005@gmail.com>
2017-05-08 16:31:17 -0600

    updated main.R

--- Lesson5/Exercise/main.R
+++ Lesson5/Exercise/main.R
@@ -15,26 +15,23 @@ list <- list.files(pattern = glob2rx('*.tif'), path = "data/wl5", full.names = T
 #Create a rasterstack of wl5
 wl5 <- stack(list)
 
-#Save the w1947 rasterstack as a single .grd file.
-#32,768 is just one number above the max value for INT2S
-#Therefore INT4S is needed.
-writeRaster(x=wl5, filename='data/wl5.grd', datatype='INT4S')
-
-#Save the .grd file as a brick variable
-WL5 <- brick('data/wl5.grd')
-
 # Repeat all the above steps for the landsat 8 image.
 # The image is from Janurary 9th, 2014
 untar("data/LC81970242014109-SC20141230042441.tar.gz", exdir = "data/wl8")
 list <- list.files(pattern = glob2rx('*.tif'), path = "data/wl8", full.names = TRUE)
 wl8 <- stack(list)
-writeRaster(x=wl8, filename='data/wl8.grd', datatype='INT4S')
-WL8 <- brick('data/wl8.grd')
 
+
+#Generate and plot the NVDI from the landsat 5 image. 
+ndvi_wl5 <- (wl5[[4]] - wl5[[3]]) / (wl5[[4]] + wl5[[3]])
+plot(ndvi_wl5)
+
+#Generate and plot the NVDI from the landsat 8 image. 
+ndvi_wl8 <- (wl8[[5]] - wl8[[4]]) / (wl8[[5]] + wl8[[4]])
+plot(ndvi_wl8)
 
 #Next Steps:
 #1: Remove clouds from both.
-#2: Generate NDVI images from both.
-#3: Crop their extents to where they both overlap. How do you crop images based
+#2: Crop their extents to where they both overlap. How do you crop images based
 #their intersection?
-#4: Subtract the 2014 one from the 1990 one and save as a new file. 
+#3: Subtract the 2014 one from the 1990 one and save as a new file. 
