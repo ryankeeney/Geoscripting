de9e5c15428fdd8a65ec656f08884633c0a66958
ryankeeney <rwk005@gmail.com>
2017-05-16 12:32:00 -0600

    update vector exercise

--- Lesson6/Exercise/main.R
+++ Lesson6/Exercise/main.R
@@ -11,14 +11,41 @@ library(raster)
 places <- readOGR("data", "places")
 railways <- readOGR("data", "railways")
 
+
+railways2 <- spTransform(railways, crs("+init=epsg:28992"))
+
+
+
 #Select only the industrial railways.
-industrial_railways <- subset(railways, type == "industrial")
+industrial_railways <-subset(railways, type == "industrial")
 
 #Reproject industrial railways to meter based coordinate system in preperation for buffering.
 #The coordinate system is Amersfoort/RD New, the Netherland's national projection
 #It's epsg number is 28992
 RD_ind_rail <- spTransform(industrial_railways, crs("+init=epsg:28992"))
 
+
 #Buffer industrial railways by 1000m
 buff_RD_ind_rail <- gBuffer(RD_ind_rail, byid = TRUE,
                          width = 1000)
+
+#Reproject places
+RD_places <- spTransform(places, crs("+init=epsg:28992"))
+
+#Get intersection of places and industrial railways.
+#stuck
+Place_Rail_Intersection <- intersect(RD_places, buff_RD_ind_rail)
+plot(Place_Rail_Intersection)
+
+
+
+
+
+
+
+
+
+
+
+
+
